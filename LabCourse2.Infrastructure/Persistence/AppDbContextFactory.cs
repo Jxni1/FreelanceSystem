6503fc8c0a8f4a1a99d3197133b2ab3951e239a3
DelvineBekteshi <db70675@ubt-uni.net>
2026-04-03 15:54:33 +0200

    Updates and related migrations

---
+++ LabCourse2.Infrastructure/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace LabCourse2.Infrastructure.Persistence
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+             
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+             
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+             
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ LabCourse2.Infrastructure/Persistence/Migrations/20260402172908_UpdateProjectTablesConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabCourse2.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProjectTablesConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Protected_Views_Projects_ProjectID",
+                table: "Protected_Views");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Protected_Views_Projects_ProjectID",
+                table: "Protected_Views",
+                column: "ProjectID",
+                principalTable: "Projects",
+                principalColumn: "ProjectID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Protected_Views_Projects_ProjectID",
+                table: "Protected_Views");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Protected_Views_Projects_ProjectID",
+                table: "Protected_Views",
+                column: "ProjectID",
+                principalTable: "Projects",
+                principalColumn: "ProjectID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
