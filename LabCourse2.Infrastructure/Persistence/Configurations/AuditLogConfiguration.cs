37267e778f55599fe24df522ecde1934676bda75
Flandra Bytyqi <flandrabytyqi93@gmail.com>
2026-04-05 12:44:01 +0200

    Added indexes for tables Notification, Settings, Audit_Logs

---
+++ LabCourse2.Infrastructure/Persistence/Configurations/AuditLogConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using LabCourse2.Domain.Entities;
+
+namespace LabCourse2.Infrastructure.Persistence.Cofigurations
+{
+    public class AuditLogConfiguration : IEntityTypeConfiguration<Audit_Logs>
+    {
+        public void Configure(EntityTypeBuilder<Audit_Logs> builder)
+        {
+            builder.HasKey(a => a.Audit_LogsID);
+
+           
+            builder.HasIndex(a => a.UserID);
+
+            
+            builder.HasIndex(a => a.Created_at);
+        }
+    }
+}
---
+++ LabCourse2.Infrastructure/Persistence/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using LabCourse2.Domain.Entities;
+
+namespace LabCourse2.Infrastructure.Persistence.Cofigurations
+{
+    public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.HasKey(n => n.NotificationID);
+
+           
+            builder.HasIndex(n => n.UserID);
+
+            
+            builder.HasIndex(n => new { n.UserID, n.IsRead });
+        }
+    }
+}
