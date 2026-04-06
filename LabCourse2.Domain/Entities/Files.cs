fb6d809f194650872e84d1a6aca7d72e468764b0
FjollaNebihi <fn70380@ubt-uni.net>
2026-04-06 14:59:57 +0200

    fixing constrains and indexes by creating specific config files for entities

--- LabCourse2.Domain/Entities/Files.cs
+++ LabCourse2.Domain/Entities/Files.cs
@@ -1,37 +1,23 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace LabCourse2.Domain.Entities
 {
     public class Files
     {
-        [Key] 
         public Guid FilesID { get; set; }
 
-        [Required]
-        [MaxLength(100)] 
         public string Entity { get; set; } = null!;
 
-        [Required] 
         public Guid EntityID { get; set; }
 
-        [Required]
-        [MaxLength(255)] 
         public string Filename { get; set; } = null!;
 
-        [Required]
-        [MaxLength(1000)] 
         public string File_Path { get; set; } = null!;
 
-        [Required]
         public long File_Size { get; set; }
 
-        [Required]
-        [MaxLength(100)] 
         public string Uploaded_by { get; set; } = null!;
 
-        [Required]
-        [MaxLength(100)]
         public string Created_by { get; set; } = null!;
     }
 }
---
+++ LabCourse2.Infrastructure/Persistence/Configurations/FilesConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using LabCourse2.Domain.Entities;
+
+namespace LabCourse2.Infrastructure.Persistence.Configurations
+{
+    public class FilesConfiguration : IEntityTypeConfiguration<Files>
+    {
+        public void Configure(EntityTypeBuilder<Files> builder)
+        {
+            builder.HasKey(f => f.FilesID);
+
+            builder.Property(f => f.Entity)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(f => f.EntityID)
+                .IsRequired();
+
+            builder.Property(f => f.Filename)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(f => f.File_Path)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(f => f.File_Size)
+                .IsRequired();
+
+            builder.Property(f => f.Uploaded_by)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(f => f.Created_by)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            
+
+            builder.HasIndex(f => new { f.Entity, f.EntityID });
+
+          
+            builder.HasIndex(f => f.Filename);
+
+          
+            builder.HasIndex(f => f.Uploaded_by);
+
+          
+
+            builder.HasCheckConstraint(
+                "CK_Files_FileSize",
+                "[File_Size] > 0"
+            );
+
+            builder.ToTable("Files");
+        }
+    }
+}
