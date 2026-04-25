45cc8dab940f68f822c5702a5bcb4d22e8e60212
FjollaNebihi <fn70380@ubt-uni.net>
2026-04-25 14:47:36 +0200

    CRUD for project's milestones

---
+++ LabCourse2.Application/DTOs/Deliverables/DeliverableResponse.cs
@@ -0,0 +1,22 @@
+﻿
+namespace LabCourse2.Application.DTOs.Deliverables
+{
+    public class DeliverableResponse
+    {
+        public Guid DeliverablesID { get; init; }
+        public DateTime Submitted_at { get; init; }
+        public DateTime? Approved_at { get; init; }
+
+        public Guid MilestoneID { get; init; }
+        public string MilestoneTitle { get; init; } = string.Empty;
+        public string MilestoneStatus { get; init; } = string.Empty;
+
+      
+        public Guid FileID { get; init; }
+        public string FileName { get; init; } = string.Empty;
+        public string FileUrl { get; init; } = string.Empty;
+
+        public bool IsApproved { get; init; }
+        public bool IsPendingReview { get; init; }
+    }
+}
---
+++ LabCourse2.Application/DTOs/Milestones/CreateMilestoneRequest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LabCourse2.Application.DTOs.Milestones
+{
+    public class CreateMilestoneRequest
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime DueDate { get; set; }
+        public Guid ContractID { get; set; }
+    }
+}
